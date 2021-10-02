ad4e4f293108bd5340fb24a4a8efd5863caa3ec7
mmmarceloo <83249188+mmmarceloo@users.noreply.github.com>
2021-10-02 10:58:20 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function abreMenu(){
+    document.body.style.setProperty('--mostraDisplay','block');
+   
+   // document.getElementById("menu-celular").style.display="block";
+    document.getElementById("menu-celular").style.right="0px";
+   
+}
